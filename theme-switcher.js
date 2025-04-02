0a997b615f6a797e2ee65c9a90f90bbfcd6a755a
BentoTPG <130327391+BentoTPG@users.noreply.github.com>
2025-04-02 09:29:58 +0700

    really cool dark theme

--- theme-switcher.js
+++ theme-switcher.js
@@ -1,11 +1,14 @@
 // ตรวจสอบธีมที่ผู้ใช้ตั้งค่า
+const body = document.body;
 function switchFavicon() {
     const favicon = document.getElementById('favicon');
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         // เปลี่ยนเป็น Favicon สำหรับ Dark Mode
         favicon.setAttribute('href','./images/favicon_invert.ico');
+        body.classList.add("dark-mode");
     } else {
         favicon.setAttribute('href','./images/favicon.ico');
+        body.classList.remove("dark-mode");
     }
 }
 
